ad0ca1f6efd7ea5acb4d4949a523f3dcbdfab066
nhpcorp35 <nhpcorp35@users.noreply.github.com>
2026-09-17 22:33:36 +0000

    Retry with correct key format

--- debug_check_vfat_baseline.py
+++ debug_check_vfat_baseline.py
@@ -2,7 +2,7 @@ import requests
 import json
 
 resp = requests.get(
-    "https://vfat.lptracker.info/api/debug/baseline/base:uniswap_v3:5992616",
+    "https://vfat.lptracker.info/api/debug/baseline/base:uniswap:5992616",
     auth=("", "J__9EXrwEA77ScMf"),
     timeout=20,
 )
